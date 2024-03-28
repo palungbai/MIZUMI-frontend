09ec6a058c035c0574a9df1bf6079df8ed028ebf
pyoucharoen <pochara.youcharoen@agoda.com>
2024-03-29 00:34:26 +0700

    feat: implemnt polling

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import AppRouter from "./routes/routes.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
     <AppRouter />
+    </QueryClientProvider>
   </React.StrictMode>
 );
--- src/pages/ResultPage.tsx
+++ src/pages/ResultPage.tsx
@@ -1,6 +1,8 @@
+import { AlertDestructive } from "@/components/ErrorAlert";
 import { ResultSkeleton } from "@/components/ResultSkeleton";
+import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
-import { useEffect, useMemo, useState } from "react";
+import { useMemo, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 
 enum UrlKey {
@@ -9,58 +11,93 @@ enum UrlKey {
   NOSUNSCREEN = "noSunscreenRefId",
 }
 
+interface ImageResponse {
+  id: string;
+  sunscreenImgUrl: string;
+  noSunscreenImgUrl: string;
+  status: "pending" | "succeeded";
+}
+
 const ResultPage = () => {
   const { search } = useLocation();
   const navigate = useNavigate();
-  const query = useMemo(() => new URLSearchParams(search), [search]);
   const [sunscreenImgUrl, setSunscreenImgUrl] = useState<string>("");
   const [noSunscreenImgUrl, setNoSunscreenImgUrl] = useState<string>("");
   const [isLoading, setIsLoading] = useState<boolean>(false);
+  const query = useMemo(() => new URLSearchParams(search), [search]);
   const id = query.get(UrlKey.ID);
   const sunscreenRefId = query.get(UrlKey.SUNSCREEN);
   const noSunscreenRefId = query.get(UrlKey.NOSUNSCREEN);
 
-  console.log(id, sunscreenRefId, noSunscreenRefId)
+  console.log(id, sunscreenRefId, noSunscreenRefId);
 
-  // if (!id || !sunscreenRefId || !noSunscreenRefId) {
-  //   navigate("/");
-  // }
+  if (!id || !sunscreenRefId || !noSunscreenRefId) {
+    navigate("/");
+  }
 
-  useEffect(() => {
-    const endpoints = `${import.meta.env.VITE_APP_API_URL as string}/api/facial-transform-poll/${id}?${UrlKey.SUNSCREEN}=${sunscreenRefId}&${UrlKey.NOSUNSCREEN}=${noSunscreenRefId}`;
-      console.log(endpoints)
+  const { error } = useQuery<ImageResponse | undefined>({
+    queryKey: ["facial-transform-poll", id],
+    queryFn: async () => {
+      const endpoints = `${
+        import.meta.env.VITE_APP_API_URL as string
+      }/api/facial-transform-poll/${id}?${UrlKey.SUNSCREEN}=${sunscreenRefId}&${
+        UrlKey.NOSUNSCREEN
+      }=${noSunscreenRefId}`;
 
-      const fetchResult = async () => {
-        try {
-          setIsLoading(true)
-          const resp = await axios.get(endpoints, {
-            headers: {
-              Authorization: `Bearer ${
-                import.meta.env.VITE_APP_API_TOKEN as string
-              }`,
-            },
-          });
-          const data = resp.data;
-          setIsLoading(false);
-          setSunscreenImgUrl(data.sunscreenImgUrl);
-          setNoSunscreenImgUrl(data.noSunscreenImgUrl);
-        } catch (error) {
-          console.error("Error fetching result: ", error);
+      try {
+        setIsLoading(true);
+        const resp = await axios.get(endpoints, {
+          headers: {
+            Authorization: `Bearer ${
+              import.meta.env.VITE_APP_API_TOKEN as string
+            }`,
+          },
+        });
+        const data: ImageResponse = resp.data;
+        setNoSunscreenImgUrl(data.noSunscreenImgUrl);
+        setSunscreenImgUrl(data.sunscreenImgUrl);
+        if (data.status === "succeeded") {
           setIsLoading(false);
         }
+        return data;
+      } catch (error) {
+        console.error("Error fetching result: ", error);
+        navigate("/");
       }
-    
-      fetchResult();
-  }, [id, noSunscreenRefId, query, sunscreenRefId]);
+    },
+    refetchInterval: (query) => {
+      console.log(query.state.data?.status);
+      return query.state.data?.status === "pending" ? 2000 : false;
+    },
+  });
 
   return (
     <div className="relative flex flex-col w-1080 h-1920 overflow-hidden bg-[url('/04-result-page/result-page-bg.png')] bg-contain ">
       <div className="absolute bg-black w-[460px] h-[666px] bottom-[444px] left-[70px] rounded-2xl z-10">
-        {isLoading ? <ResultSkeleton /> : <img src={sunscreenImgUrl} alt="result" height='460px' width='460px' />}
+        {isLoading ? (
+          <ResultSkeleton />
+        ) : (
+          <img
+            src={noSunscreenImgUrl}
+            alt="result"
+            height="460px"
+            width="460px"
+          />
+        )}
       </div>
       <div className="absolute bg-black w-[460px] h-[666px] bottom-[444px] right-[70px] rounded-2xl z-10">
-      {isLoading ? <ResultSkeleton /> : <img src={noSunscreenImgUrl} alt="result" height='460px' width='460px' />}
+        {isLoading ? (
+          <ResultSkeleton />
+        ) : (
+          <img
+            src={sunscreenImgUrl}
+            alt="result"
+            height="460px"
+            width="460px"
+          />
+        )}
       </div>
+      {error && <AlertDestructive />}
     </div>
   );
 };
