c16689c11c49283c91fffc47ddc50fdbf125980b
itsarasib <itsara.pliansart@gmail.com>
2024-03-30 04:29:09 +0700

    Refactor: countdown timer

--- src/components/CountdownTimer.tsx
+++ src/components/CountdownTimer.tsx
@@ -9,13 +9,13 @@ const CountdownTimer: React.FC<CountdownTimerProps> = ({
 }) => {
   return (
     <div>
-      {isCounting && remaining > 0 ? (
+      {isCounting && remaining > 0 && (
         <img
           src={`/02-capture-page/${remaining}.svg`}
           alt={`Countdown ${remaining}`}
           className="w-1/4 h-1/4 absolute bottom-[955px] left-[404px] z-30"
         />
-      ) : null}
+      )}
     </div>
   );
 };
