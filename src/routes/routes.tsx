ea173f2820fc78131bdce3a253d5b15cb9f239e0
pyoucharoen <pochara.youcharoen@agoda.com>
2024-03-30 02:00:59 +0700

    fix: remove loading route

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,6 +1,5 @@
 import CapturePage from "@/pages/CapturePage";
 import HomePage from "@/pages/Home";
-import { LoadingPage } from "@/pages/LoadingPage";
 import ResultPage from "@/pages/ResultPage";
 
 import { BrowserRouter, Route, Routes } from "react-router-dom";
@@ -12,7 +11,6 @@ const AppRouter = () => {
         <Route path="/" element={<HomePage />} />
         <Route path="/capture" element={<CapturePage />} />
         <Route path="/result" element={<ResultPage />} />
-        <Route path="/loading" element={<LoadingPage />} />
       </Routes>
     </BrowserRouter>
   );
