ae7e0a3eb09f131c27f3519b2f1967fa84485a31
pyoucharoen <pochara.youcharoen@agoda.com>
2024-03-31 01:55:53 +0700

    Refactor: code format

--- src/components/ui/VideoRecorder.tsx
+++ src/components/ui/VideoRecorder.tsx
@@ -1,58 +1,63 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import axios from "axios";
 import { useCallback, useEffect } from "react";
 import { useReactMediaRecorder } from "react-media-recorder";
 
 const VideoRecorder = () => {
-    const { status, startRecording, stopRecording, mediaBlobUrl } =
-      useReactMediaRecorder({ video: true });
+  const { status, startRecording, stopRecording, mediaBlobUrl } =
+    useReactMediaRecorder({ video: true });
+    const VIDEO_DURATION = 5000;
 
-    useEffect(() => {
-        const handleRecord = async () => {
-            startRecording();
-            await new Promise((resolve) => setTimeout(resolve, 5000));
-            stopRecording();
-        }
-        handleRecord()
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [])
+  useEffect(() => {
+    const handleRecord = async () => {
+      startRecording();
+      await new Promise((resolve) => setTimeout(resolve, VIDEO_DURATION));
+      stopRecording();
+    };
+    handleRecord();
+  }, []);
 
-    const handleSubmitVideo = useCallback(async () => {
-        if (!mediaBlobUrl) return;
+  const handleSubmitVideo = useCallback(async () => {
+    if (!mediaBlobUrl) return;
 
-        try {
-          const blob = await fetch(mediaBlobUrl).then((r) => r.blob());
+    try {
+      const blob = await fetch(mediaBlobUrl).then((r) => r.blob());
 
-        const formData = new FormData();
-        formData.append('file', blob, `video-${Date.now()}.webm`);
-        await axios.post(`${import.meta.env.VITE_APP_API_URL}/api/video-polling`, formData, {
+      const formData = new FormData();
+      formData.append("file", blob, `video-${Date.now()}.webm`);
+      await axios.post(
+        `${import.meta.env.VITE_APP_API_URL}/api/video-polling`,
+        formData,
+        {
           headers: {
             "Content-Type": "multipart/form-data",
-            "Authorization": `Bearer ${
+            Authorization: `Bearer ${
               import.meta.env.VITE_APP_API_TOKEN as string
             }`,
-          }
-        })
-        } catch (error) {
-          console.error("Error uploading video: ", error);
+          },
         }
-    }, [mediaBlobUrl])
+      );
+    } catch (error) {
+      console.error("Error uploading video: ", error);
+    }
+  }, [mediaBlobUrl]);
 
-    useEffect(() => {
-        if (!mediaBlobUrl) return;
+  useEffect(() => {
+    if (!mediaBlobUrl) return;
 
-        handleSubmitVideo();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [mediaBlobUrl])
-  
-    return (
-      <>
+    console.log(mediaBlobUrl);
+    // handleSubmitVideo();
+  }, [mediaBlobUrl]);
+
+  return (
+    <>
       <div className="hidden">
         <p>{status}</p>
         <button onClick={startRecording}>Start Recording</button>
         <button onClick={stopRecording}>Stop Recording</button>
       </div>
-      </>
-    );
-  };
+    </>
+  );
+};
 
-export { VideoRecorder }
+export { VideoRecorder };
