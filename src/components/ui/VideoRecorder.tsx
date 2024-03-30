67ac9a9cf48868b239a4489dc067cc1b4834fa19
pyoucharoen <pochara.youcharoen@agoda.com>
2024-03-31 01:21:57 +0700

    feat: handle upload video

--- src/components/ui/VideoRecorder.tsx
+++ src/components/ui/VideoRecorder.tsx
@@ -1,4 +1,5 @@
-import { useEffect } from "react";
+import axios from "axios";
+import { useCallback, useEffect } from "react";
 import { useReactMediaRecorder } from "react-media-recorder";
 
 const VideoRecorder = () => {
@@ -15,10 +16,32 @@ const VideoRecorder = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
 
+    const handleSubmitVideo = useCallback(async () => {
+        if (!mediaBlobUrl) return;
+
+        try {
+          const blob = await fetch(mediaBlobUrl).then((r) => r.blob());
+
+        const formData = new FormData();
+        formData.append('file', blob, `video-${Date.now()}.webm`);
+        await axios.post(`${import.meta.env.VITE_APP_API_URL}/api/video-polling`, formData, {
+          headers: {
+            "Content-Type": "multipart/form-data",
+            "Authorization": `Bearer ${
+              import.meta.env.VITE_APP_API_TOKEN as string
+            }`,
+          }
+        })
+        } catch (error) {
+          console.error("Error uploading video: ", error);
+        }
+    }, [mediaBlobUrl])
+
     useEffect(() => {
         if (!mediaBlobUrl) return;
 
-        console.log(mediaBlobUrl)
+        handleSubmitVideo();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [mediaBlobUrl])
   
     return (
