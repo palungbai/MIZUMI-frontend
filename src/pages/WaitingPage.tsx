08ea5817a891649d713d9b7d224c343232108822
pyoucharoen <pochara.youcharoen@agoda.com>
2024-03-31 04:39:29 +0700

    Refactor: remove unused page

--- src/pages/WaitingPage.tsx
+++
@@ -1,7 +0,0 @@
-const WaitingPage = () => {
-  return (
-    <div className="relative flex flex-col w-1080 h-1920 overflow-hidden bg-[url('/03-waiting-page/waiting-bg.png')] bg-contain  "></div>
-  );
-};
-
-export default WaitingPage;
