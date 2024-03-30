d96997983c906735a25001c48a22d87c3af628c3
itsarasib <itsara.pliansart@gmail.com>
2024-03-31 00:06:20 +0700

    fix: handle button

--- src/pages/DisplayPage.tsx
+++ src/pages/DisplayPage.tsx
@@ -4,17 +4,23 @@ import { useNavigate } from "react-router-dom";
 const DisplayPage = () => {
   const sunscreenUrl = localStorage.getItem(UrlKey.URL);
   const navigate = useNavigate();
-  
+
   if (!sunscreenUrl) {
     navigate("/");
-    return
+    return;
   }
 
   return (
     <>
-      <div className="relative flex flex-col w-1080 h-1920 overflow-hidden bg-[url('/05-display-page/display-page-bg.png')] bg-contain -z-30">
-        <div className='absolute w-[810px] h-[810px] bottom-[360px] left-[140px] rounded-2xl -z-20'>
-          <img src={sunscreenUrl} alt='no sunscreen' width="810px" height="810px" className="rounded-2xl" />
+      <div className="relative flex flex-col w-1080 h-1920 overflow-hidden bg-[url('/05-display-page/display-page-bg.png')] bg-contain">
+        <div className="absolute w-[810px] h-[810px] bottom-[360px] left-[140px] rounded-2xl z-10">
+          <img
+            src={sunscreenUrl}
+            alt="no sunscreen"
+            width="810px"
+            height="810px"
+            className="rounded-2xl"
+          />
         </div>
 
         <img
