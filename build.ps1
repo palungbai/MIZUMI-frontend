4abcb98dabaea2b01ab6ad293cc494b1c24c0a71
itsarasib <itsara.pliansart@gmail.com>
2024-04-13 01:37:07 +0700

    delete: build ps1

--- build.ps1
+++
@@ -1,1 +0,0 @@
-nativefier --icon './MIZUMI-frontend/public/mizumi.ico' --name 'Mizumi' --disable-context-menu --full-screen --hide-window-frame --disable-dev-tools 'https://mizumi-frontend.vercel.app?location={location}'
