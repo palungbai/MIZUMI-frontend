ea8e9360c45defa8dfb95d8569847c95e2d512e7
itsarasib <itsara.pliansart@gmail.com>
2024-04-24 22:41:40 +0700

    fix: home page layout

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,59 +1,3 @@
-// import { ConsentModal } from "@/components/ConsentModal";
-// import { LoadingPage } from "./LoadingPage";
-// import { useLoadBg } from "@/hooks/useLoadBg";
-
-// const HomePage = () => {
-//   const { isBgLoaded } = useLoadBg("/01-home-page/homePageBg.png");
-
-//   if (!isBgLoaded) return <LoadingPage />;
-
-//   return (
-//     <div className="relative flex flex-col overflow-hidden w-1080 h-1920 bg-[url('/01-home-page/homePageBg.png')]">
-//       <img
-//         src="../01-home-page/ParagonLogo.png"
-//         alt="product"
-//         className="absolute z-30 w-[130px] left-[346px] top-[28px] rounded-lg scale-90"
-//       />
-
-//       <img
-//         src="../01-home-page/product.png"
-//         alt="product"
-//         className="absolute z-30"
-//       />
-
-//       <img
-//         src="../01-home-page/ball-crab.png"
-//         alt="carb"
-//         className="absolute z-30"
-//       />
-
-//       <div className="absolute bottom-[535px] left-[-54px] rotate-[5deg] border-[16px] border-white rounded-3xl">
-//         <img
-//           src="../01-home-page/present-model.png"
-//           alt="present-model"
-//           width={450}
-//           height={600}
-//         />
-//       </div>
-
-//       <div className="absolute bottom-[496px] right-[-60px] rotate-[-10deg] border-[16px] border-white rounded-3xl">
-//         <img
-//           src="../01-home-page/future-model.png"
-//           alt="future-model"
-//           width={450}
-//           height={600}
-//         />
-//       </div>
-
-//       <div className="absolute bottom-60 left-0 flex justify-center w-full z-40">
-//         <ConsentModal />
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default HomePage;
-
 import { ConsentModal } from "@/components/ConsentModal";
 import { LoadingPage } from "./LoadingPage";
 import { useLoadBg } from "@/hooks/useLoadBg";
@@ -80,7 +24,7 @@ const HomePage = () => {
         alt="product"
         className="absolute z-30"
       />
-      s
+
       <img
         src="../01-home-page/present.png"
         alt="product"
